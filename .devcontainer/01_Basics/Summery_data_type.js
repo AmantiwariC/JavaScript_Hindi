14e9cdd53c8b3351bb3bbf0c35a15882511722ae
AmantiwariC <116155132+AmantiwariC@users.noreply.github.com>
2024-04-12 08:03:12 +0000

    data_type and return type

---
+++ .devcontainer/01_Basics/Summery_data_type.js
@@ -0,0 +1,33 @@
+//there is total two type of data types 1. Primitive(call by value) 2. non primitive (call by reference )
+/* let us understand Primitive data types 
+  1.Number 
+  2.BigInt
+  3.String
+  4. boolean => true/false
+  5. null => standalone value
+  6. undefined => 
+  7. symbol => unique
+  */
+ // non primitive data types are: object ,Array , function
+ let hero=['ben 10', 'Power Ranger ','Saktiman']
+ console.log(hero)
+ let Fun_name =function(){
+    console.log("AMan")
+ }
+ console.log(typeof Fun_name)
+ /*
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+  */     
