1ba51187340c3d87c846b8e175dd297b43ec34ff
AmantiwariC <116155132+AmantiwariC@users.noreply.github.com>
2024-04-18 09:28:32 +0000

    Scope in js

---
+++ .devcontainer/03_basics/Scope.js
@@ -0,0 +1,22 @@
+function one(){
+    const name ="amamn";
+    function two (){
+        const  web ="fb"
+        console.log(name)
+    }
+    // console.log(web)
+    two()
+}
+one()
+//+++++++++++++++++++++++++++++++++++++++++intresting concept++++++++++++
+console.log(addone(7))
+function addone(num){
+    return num+1
+}
+// expressiing technique
+// you can't log add two fn value
+
+const addtwo = function(num){
+    return num + 2
+}
+console.log(addtwo(1))
